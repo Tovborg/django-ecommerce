01326966b516486d392db532ddd86a18b89653d7
Tovborg <emiltovborg11@gmail.com>
2022-06-08 16:50:52 +0200

    fixed

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -15,17 +15,19 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # migrations.CreateModel(
-        #     name='BillingAddress',
-        #     fields=[
-        #         ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-        #         ('street_address', models.CharField(max_length=100)),
-        #         ('apartment_address', models.CharField(max_length=100)),
-        #         ('country', django_countries.fields.CountryField(max_length=2)),
-        #         ('zip', models.CharField(max_length=100)),
-        #         ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-        #     ],
-        # ),
+        migrations.CreateModel(
+            name='BillingAddress',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True,
+                                           primary_key=True, serialize=False, verbose_name='ID')),
+                ('street_address', models.CharField(max_length=100)),
+                ('apartment_address', models.CharField(max_length=100)),
+                ('country', django_countries.fields.CountryField(max_length=2)),
+                ('zip', models.CharField(max_length=100)),
+                ('user', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
         migrations.CreateModel(
             name='Category',
             fields=[
